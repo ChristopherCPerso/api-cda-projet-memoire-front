76c8a9121786a62cd89cca0fcd49ad9fcd061d06
Christopher Chiarandini <chiarandini.christopher@gmail.com>
2025-08-30 09:46:06 +0200

    add site version in footer

--- app/components/ui/Footer.tsx
+++ app/components/ui/Footer.tsx
@@ -1,8 +1,9 @@
 import { Link } from "react-router";
+import packJson from "../../../package.json";
 
 export default function Footer() {
   const logo = "/img/logo_atecna_big.png";
-
+  const version = packJson.version;
   return (
     <footer className="bg-blue-night grid min-h-[300px] grid-cols-4 text-white">
       <div className="relative overflow-hidden">
@@ -19,27 +20,30 @@ export default function Footer() {
         &copy;Atecna - 2025
       </div>
 
-      <div className="font-open flex justify-end self-center p-6">
-        <ul className="space-y-3">
-          <li>
-            {" "}
-            <Link className="hover:underline" to="/">
-              Restaurant
-            </Link>
-          </li>
-          <li>
-            {" "}
-            <Link className="hover:underline" to="/">
-              Ajouter un restaurant
-            </Link>
-          </li>
-          <li>
-            {" "}
-            <Link className="hover:underline" to="/">
-              Contactez-nous
-            </Link>
-          </li>
-        </ul>
+      <div className="flex flex-col justify-between text-end">
+        <div className="font-open flex h-full w-full items-center justify-end p-6">
+          <ul className="space-y-3">
+            <li>
+              {" "}
+              <Link className="hover:underline" to="/">
+                Restaurant
+              </Link>
+            </li>
+            <li>
+              {" "}
+              <Link className="hover:underline" to="/">
+                Ajouter un restaurant
+              </Link>
+            </li>
+            <li>
+              {" "}
+              <Link className="hover:underline" to="/">
+                Contactez-nous
+              </Link>
+            </li>
+          </ul>
+        </div>
+        <div className="p-2 px-6 text-white/15">{version}</div>
       </div>
     </footer>
   );
